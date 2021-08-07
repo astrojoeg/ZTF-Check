5183885daa7d70494f67a94e1bbf788474052ea9
Joseph Guidry <josephguidry@utexas.edu>
2021-08-07 11:57:40 -0500

    updated version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Joseph Guidry'
 master_doc='index'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
