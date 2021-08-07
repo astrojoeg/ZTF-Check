7f837e79d61cd5901ca7b76590b9c351b7daf1fd
Joseph Guidry <josephguidry@utexas.edu>
2021-08-07 09:43:07 -0500

    updates

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
 setup(
-	name=”ZTF-Check”,
-	version=”0.1”,
-	packages=find_packages()
+	name="ZTF-Check",
+	version="0.1",
+	author='Joseph Guidry',
+	author_email="jaguidry@bu.edu",
+	description="ZTF Check! The quick way to check out your target's FOV in ZTF.",
+    url='https://github.com/astrojoeg/ZTF-Check',
+    install_requires=requirements,
+	packages=find_packages(),
+	entry_points ={
+            'console_scripts': [
+                'ztfcheck = ztfcheck'
+            ]
+        },
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Astronomy"
+    ]
 )
